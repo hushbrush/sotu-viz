9f49135949fe7ffcba4730943e156f90de70c4ef
hushbrush <harshitachakravadhanula@gmail.com>
2025-01-13 04:27:47 +0530

    esnect

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
+    build: {
+        target: 'esnext', // Ensures modern features like top-level await are supported
+      },
+  
   base: '/sotu-viz/'
 })
